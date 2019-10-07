aa68e47dc27b0f1f3072e32697afc0ec6157469b
kiran-golang <kirankamineni@outlook.com>
2019-10-06 23:30:21 -0700

    Bug in build.sh
    Signed-off-by: kiran-golang <kirankamineni@outlook.com>

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-CGO_ENABLED=0
-GOARCH=amd64
-GOOS=linux
+export CGO_ENABLED=0
+export GOARCH=amd64
+export GOOS=linux
 
 go build -o jfrog-stats -v main.go
 go test -v -cover ./...
